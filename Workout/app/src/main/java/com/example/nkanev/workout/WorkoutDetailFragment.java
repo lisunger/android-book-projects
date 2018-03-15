039fa1e1f22c3e926402ed04145fad2a302ba355
n_kanev <nikolay.r.kanev@gmail.com>
2018-03-15 12:14:35 +0200

    Added code from chapter 11

--- Workout/app/src/main/java/com/example/nkanev/workout/WorkoutDetailFragment.java
+++ Workout/app/src/main/java/com/example/nkanev/workout/WorkoutDetailFragment.java
@@ -3,6 +3,7 @@ package com.example.nkanev.workout;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,20 @@ public class WorkoutDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container, stopwatch);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        } else {
+            workoutId = savedInstanceState.getLong("workoutId");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
