0f261e8a36c991041561c1929528f698e4f43025
n_kanev <nikolay.r.kanev@gmail.com>
2018-03-16 17:08:15 +0200

    bits and pizzas updates

--- BitsandPizzas/app/src/main/java/com/example/nkanev/bitsandpizzas/OrderActivity.java
+++ BitsandPizzas/app/src/main/java/com/example/nkanev/bitsandpizzas/OrderActivity.java
@@ -1,5 +1,6 @@
 package com.example.nkanev.bitsandpizzas;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,7 +11,11 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
