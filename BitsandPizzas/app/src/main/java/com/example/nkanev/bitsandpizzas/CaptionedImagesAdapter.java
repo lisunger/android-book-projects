ec0ed0e3bc330fa115e029224d3f1c4301068c26
n_kanev <nikolay.r.kanev@gmail.com>
2018-03-21 11:42:25 +0200

    added click events to the pizzas

--- BitsandPizzas/app/src/main/java/com/example/nkanev/bitsandpizzas/CaptionedImagesAdapter.java
+++ BitsandPizzas/app/src/main/java/com/example/nkanev/bitsandpizzas/CaptionedImagesAdapter.java
@@ -1,10 +1,12 @@
 package com.example.nkanev.bitsandpizzas;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,15 +15,25 @@ class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter
 
     private String[] captions;
     private int[] imageIds;
+    private Listener listener;
+
+    interface Listener {
+        void onClick(int position);
+    }
 
     public CaptionedImagesAdapter(String[] captions, int[] imageIds) {
         this.captions = captions;
         this.imageIds = imageIds;
     }
 
+
     @Override
     public int getItemCount() {
         return captions.length;
+    }
+
+    public void setListener(Listener listener){
+        this.listener = listener;
     }
 
     @Override
@@ -34,15 +46,26 @@ class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, final int position) {
         CardView cardView = holder.cardView;
+
         ImageView imageView = (ImageView) cardView.findViewById(R.id.info_image);
-        Drawable drawable =
-                ContextCompat.getDrawable(cardView.getContext(), imageIds[position]);
+        Drawable drawable = ContextCompat.getDrawable(cardView.getContext(), imageIds[position]);
+
         imageView.setImageDrawable(drawable);
         imageView.setContentDescription(captions[position]);
+
         TextView textView = (TextView) cardView.findViewById(R.id.info_text);
         textView.setText(captions[position]);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onClick(position);
+                }
+            }
+        });
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
--- BitsandPizzas/app/src/main/java/com/example/nkanev/bitsandpizzas/PizzaFragment.java
+++ BitsandPizzas/app/src/main/java/com/example/nkanev/bitsandpizzas/PizzaFragment.java
@@ -1,6 +1,7 @@
 package com.example.nkanev.bitsandpizzas;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -12,10 +13,11 @@ import android.view.ViewGroup;
 public class PizzaFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         RecyclerView pizzaRecycler = (RecyclerView) inflater.inflate(
                 R.layout.fragment_pizza, container, false);
+
         String[] pizzaNames = new String[Pizza.pizzas.length];
         for (int i = 0; i < pizzaNames.length; i++) {
             pizzaNames[i] = Pizza.pizzas[i].getName();
@@ -24,10 +26,21 @@ public class PizzaFragment extends Fragment {
         for (int i = 0; i < pizzaImages.length; i++) {
             pizzaImages[i] = Pizza.pizzas[i].getImageResourceId();
         }
+
         CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pizzaNames, pizzaImages);
         pizzaRecycler.setAdapter(adapter);
+
         GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
         pizzaRecycler.setLayoutManager(layoutManager);
+
+        adapter.setListener(new CaptionedImagesAdapter.Listener() {
+            public void onClick(int position) {
+                Intent intent = new Intent(getActivity(), PizzaDetailActivity.class);
+                intent.putExtra(PizzaDetailActivity.EXTRA_PIZZA_ID, position);
+                getActivity().startActivity(intent);
+            }
+        });
+
         return pizzaRecycler;
     }
 
