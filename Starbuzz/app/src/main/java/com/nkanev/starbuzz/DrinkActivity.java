458bff7a859cc7cc9adf0f045f69665c9d4c2e84
lisunger <>
2018-03-23 16:06:08 +0200

    added favourite drinks to Starbuzz

--- Starbuzz/app/src/main/java/com/nkanev/starbuzz/DrinkActivity.java
+++ Starbuzz/app/src/main/java/com/nkanev/starbuzz/DrinkActivity.java
@@ -1,11 +1,14 @@
 package com.nkanev.starbuzz;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,10 +28,10 @@ public class DrinkActivity extends Activity {
         Cursor cursor = null;
         try {
             db = starbuzzDatabaseHelper.getReadableDatabase();
-            cursor = db.query ("DRINK",
-                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+            cursor = db.query("DRINK",
+                    new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID", "FAVORITE"},
                     "_id = ?",
-                    new String[] {Integer.toString(drinkId)},
+                    new String[]{Integer.toString(drinkId)},
                     null, null, null);
 
             if (cursor.moveToFirst()) {
@@ -36,15 +39,19 @@ public class DrinkActivity extends Activity {
                 String descriptionText = cursor.getString(1);
                 int photoId = cursor.getInt(2);
 
-                TextView name = (TextView)findViewById(R.id.name);
+                TextView name = (TextView) findViewById(R.id.name);
                 name.setText(nameText);
 
-                TextView description = (TextView)findViewById(R.id.description);
+                TextView description = (TextView) findViewById(R.id.description);
                 description.setText(descriptionText);
 
-                ImageView photo = (ImageView)findViewById(R.id.photo);
+                ImageView photo = (ImageView) findViewById(R.id.photo);
                 photo.setImageResource(photoId);
                 photo.setContentDescription(nameText);
+
+                boolean isFavorite = (cursor.getInt(3) == 1);
+                CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
+                favorite.setChecked(isFavorite);
             }
 
         } catch (SQLiteException e) {
@@ -55,6 +62,25 @@ public class DrinkActivity extends Activity {
         } finally {
             cursor.close();
             db.close();
+        }
+    }
+
+    public void onFavoriteClicked(View view) {
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
+        CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("FAVORITE", favorite.isChecked());
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = starbuzzDatabaseHelper.getWritableDatabase();
+            db.update("DRINK",
+                    drinkValues,
+                    "_id = ?",
+                    new String[]{Integer.toString(drinkId)});
+            db.close();
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
         }
     }
 }
